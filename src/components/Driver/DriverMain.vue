dd36a623471bad88f5df9f39eadf55679d43afa9
Zendy Lim <zendy.me@gmail.com>
2018-03-24 20:28:28 +0900

    commit

--- src/components/Driver/DriverMain.vue
+++ src/components/Driver/DriverMain.vue
@@ -1,69 +1,64 @@
 <template>
-  <div>
-    <mt-header title="New Schedules">
-    </mt-header>
-    <div class="container">
-      <div>Pick Up Address</div>
-      <div>{PLACEHOLDER HERE}</div>
-      <div>
-        <mt-button class="button-80" size="small" type="primary">Choose Address</mt-button>
-      </div>
+  <div class="main">
+    <mt-header title="New Schedules"></mt-header>
+    <b-container>
+      <b-row>
+        <b-col cols="12">HEHEH</b-col>
+        <b-col cols="2">Food</b-col>
+        <b-col cols="2">Liquid</b-col>
+        <b-col cols="2">Tableware</b-col>
+        <b-col cols="2">Furniture</b-col>
+        <b-col cols="2">Cloth</b-col>
+        <b-col cols="2">Other</b-col>
+        <b-col cols="12">SSSSSSSSSS<mt-switch style="float: right;" v-model="value"></mt-switch></b-col>
+        <b-col cols="3">
+          FROM
+        </b-col>
+        <b-col cols="9">
+          <b-row>
+            <b-col cols="12">Something</b-col>
+            <b-col cols="12">Something</b-col>
+          </b-row>
+        </b-col>
+        <b-col cols="3">
+          TO
+        </b-col>
+        <b-col cols="9">
+          <b-row>
+            <b-col cols="12">Something</b-col>
+            <b-col cols="12">Something</b-col>
+          </b-row>
+        </b-col>
+        <b-col cols="12">
+          <b-form-group label="Something">
+            <b-form-radio-group id="btnradios1"
+                                buttons
+                                v-model="selected"
+                                :options="options"
+                                name="radiosBtnDefault" />
+          </b-form-group>
+        </b-col>
+        <b-col cols="12">
+          <b-form-group label="Something">
+            <b-form-radio-group id="btnradios1"
+                                buttons
+                                v-model="selected"
+                                :options="options"
+                                name="radiosBtnDefault" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div class="map">
+      MAP HERE
     </div>
-    <i class="fa fa-arrow-down"></i>
-    <div class="container">
-      <div>Drop Off Address</div>
-      <div>{PLACEHOLDER HERE}</div>
-      <div>
-        <mt-button class="button-80" size="small" type="primary">Choose Address</mt-button>
-      </div>
-    </div>
-    <hr/>
-    <table class="w-100">
-      <tr>
-        <td class="big-td">
-          <div>
-            Pick Up Time
-          </div>
-          <div>
-            {PLACEHOLDER HERE}
-          </div>
-          <div>
-            <mt-button class="button" v-on:click="startTimeOpen" type="primary" size="small">Set Time</mt-button>
-          </div>
-          <mt-datetime-picker
-            ref="picker"
-            type="datetime"
-            v-model="pickUpTime">
-          </mt-datetime-picker>
-        </td>
-        <td class="small-td">
-          ~
-        </td>
-        <td class="big-td">
-          <div>
-            Drop Off Time
-          </div>
-          <div>
-            {PLACEHOLDER HERE}
-          </div>
-          <div>
-            <mt-button class="button" type="primary" size="small">Set Time</mt-button>
-          </div>
-          <mt-datetime-picker
-            ref="picker2"
-            type="datetime"
-            v-model="pickUpTime">
-          </mt-datetime-picker>
-        </td>
-      </tr>
-    </table>
-    <mt-button style="bottom: 0; left: 0; position: absolute; width: 100%;" type="primary">GO</mt-button>
+    <mt-button class="footer" type="primary"> Onegai</mt-button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'DriverMain',
+    name: 'CustomerMain',
     methods: {
       startTimeOpen() {
         console.log(this.$refs);
@@ -72,39 +67,34 @@
     },
     data() {
       return {
-        msg: 'Chicken',
-        pickUpTime: ''
+        value: false,
+        selected: 'radio1',
+        options: [
+          { text: 'Radio 1', value: 'radio1' },
+          { text: 'Radio 3', value: 'radio2' },
+          { text: 'Radio 4', value: 'radio4' }
+        ]
       }
     }
   }
 </script>
 
 <style scoped>
-  div {
-    margin: 0.5em 0;
+  .main {
+    height: 100%;
+    padding-bottom: 46px;
   }
 
-  .button-80 {
-    width: 80%;
+  .map {
+    height: 250px;
+    background: azure;
   }
 
-  .container {
-    margin: 1em 0;
-  }
-
-  .big-td {
-    width: 45%;
-  }
-
-  .small-td {
-    width: 10%;
-  }
-
-  .fa {
-    margin-bottom: 1em;
-  }
-
-  .w-100 {
+  .footer {
+    height: 46px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
     width: 100%;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { DatetimePicker, Header, PaletteButton } from 'mint-ui';
+import { DatetimePicker, Header, Switch } from 'mint-ui';
 import Vue from 'vue'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
@@ -15,7 +15,7 @@ Vue.use(MintUI);
 Vue.use(BootstrapVue);
 Vue.component('mt-datetime-picker', DatetimePicker);
 Vue.component('mt-header', Header);
-Vue.component('mt-pallete-button', PaletteButton);
+Vue.component('mt-switch', Switch);
 
 /* eslint-disable no-new */
 new Vue({
