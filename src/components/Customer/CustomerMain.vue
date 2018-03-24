b042ed69e517fa7e70cffcb03a6344c852a227b9
Zendy Lim <zendy.me@gmail.com>
2018-03-24 20:13:19 +0900

    commit

--- src/components/Customer/CustomerMain.vue
+++ src/components/Customer/CustomerMain.vue
@@ -42,53 +42,10 @@
         </b-col>
       </b-row>
     </b-container>
-    <!--<div class="message-container">{Placeholder text}</div>
-    <div class="section">
-      <div>
-        <div class="label-container">From</div>
-      </div>
-      <div>
-        <div class="form-container">
-          <div class="form-input">Test</div>
-          <div class="form-input">Test</div>
-        </div>
-      </div>
-    </div>
-    <div class="section">
-      <div>
-        <div class="label-container">To</div>
-      </div>
-      <div>
-        <div class="form-container">
-          <div class="form-input">Test</div>
-          <div class="form-input">Test</div>
-        </div>
-      </div>
-    </div>
-    <div class="section">
-      {Some more placeholder text}
-      <div>
-        <ul class="selection">
-          <li class="selection-item">A</li>
-          <li class="selection-item">B</li>
-          <li class="selection-item">C</li>
-        </ul>
-      </div>
-    </div>
-    <div class="section">
-      {Some more placeholder text}
-      <div>
-        <ul class="selection">
-          <li class="selection-item">A</li>
-          <li class="selection-item">B</li>
-          <li class="selection-item">C</li>
-        </ul>
-      </div>
-    </div>
     <div class="map">
       MAP HERE
     </div>
-    <mt-button class="footer" type="primary"> Onegai</mt-button>-->
+    <mt-button class="footer" type="primary"> Onegai</mt-button>
   </div>
 </template>
 
@@ -118,50 +75,6 @@
   .main {
     height: 100%;
     padding-bottom: 46px;
-  }
-
-  .section {
-    margin: 6px 0;
-    height: 60px;
-  }
-
-  .message-container {
-    padding: 5px;
-  }
-
-  .label-container {
-    width: 30%;
-    background: aqua;
-    height: 60px;
-    line-height: 60px;
-    float: left;
-  }
-
-  .form-container {
-    width: 70%;
-    background: aquamarine;
-    float: right;
-    height: 60px;
-  }
-
-  .form-input {
-    height: 24px;
-    padding: 3px;
-  }
-
-  .selection {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-  }
-
-  .selection-item {
-    display: inline-block;
-    padding: 6px 0;
-    width: 33%;
-    margin: 0;
-    border: 2px black solid;
   }
 
   .map {
